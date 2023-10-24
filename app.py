75c863a3a3d3a9e04724c78faa58402e34fbb6e2
shriyab <shriya.B789@gmail.com>
2023-10-23 22:36:25 -0500

    debugged checkin and checkout

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 import pymongo
 
-from flask import Flask, jsonify
-from flask_cors import cross_origin, CORS
+from flask import Flask, jsonify, url_for, redirect, request
+#from flask_cors import cross_origin, CORS
 
-app = Flask(__name__, static_folder='./build', static_url_path='/')
-cors = CORS(app)
+app = Flask(__name__)
+#app = Flask(__name__, static_folder='./build', static_url_path='/')
+#cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
@@ -16,22 +17,20 @@ users = userDB.get_collection('Users')
 projectDB = client.get_database('Projects')
 projects = projectDB.get_collection('Project')
 
-def checkUser(username, password):
+@app.route('/user/<username>')
+def checkUser(username):
     for document in users.find():
         name = document["username"]
         passwrd = document["pass"]
-        if username == name and password == passwrd:
+        if username == name:
             print("login success!")
-            return 1
-        elif username == name:
-            #should send a message to the frontend to indicate wrong password was entered but user exists
-            print("Wrong password")
-            return 0
+            return 'login success'
         else:
             #should send a message to the frontend that user doesn't exist & to create new account
             print("user doesn't exist")
-            return 0
-        
+            return 'user does not exist'
+
+@app.route('/user/<username>')
 def addUser(username, password):
     for document in users.find():
         name = document["username"]
@@ -46,6 +45,7 @@ def addUser(username, password):
     }
     users.insert_one(document)
 
+@app.route('/getProject/<int:id>')
 def checkProject(id):
     for document in projects.find():
         projID = document["projectID"]
@@ -58,6 +58,7 @@ def checkProject(id):
             print("project with this id doesn't exist")
             return 0
 
+@app.route('/createProject/<int:id>')
 def createProject(id, description, name, hwSet1Cap, hwSet2Cap):
     for document in projects.find():
         projID = document["projectID"]
@@ -74,48 +75,86 @@ def createProject(id, description, name, hwSet1Cap, hwSet2Cap):
         "hwSet2Capacity": hwSet2Cap,
         "hwSet2Availability": hwSet2Cap
     }
+    projects.insert_one(document)
 
+@app.route('/checkOut/<int:projectID>')
 def checkOut(projectID, set, qnt, username):
     for document in projects.find():
         projID = document["projectID"]
         if projID == projectID:
             if set == 1:
-                if document["hwSet1Availability"] > qnt:
-                    document["hwSet1Availabilty"] = 0
+                if document["hwSet1Availability"] < qnt:
+                    leftover = document["hwSet1Availability"]
+                    filter = {"projectID": projectID}
+                    update = {
+                        "$set": {"hwSet1Availability": 0}
+                    }
+                    projects.update_one(filter, update)
                     for doc in users.find():
-                        user = document["username"]
+                        user = doc["username"]
                         if user == username:
-                            document["requests"] = [projectID, set, qnt]
+                            filter = {"username": username}
+                            update = {
+                                "$push": {"requests": [[projectID, "checkout", set, leftover]]}
+                            }
+                            users.update_one(filter, update)
                     #should send message to frontend that quantity requested is more than that available
                     print("quantity requested is greater than that available, checked out all remaining units")
                     return 0
                 else:
-                    document["hwSet1Availability"] -= qnt
+                    currAvail = document["hwSet1Availability"]
+                    filter = {"projectID": projectID}
+                    update = {
+                        "$set": {"hwSet1Availability": currAvail - qnt}
+                    }
+                    projects.update_one(filter, update)
                     for doc in users.find():
                         user = doc["username"]
                         if user == username:
-                            doc["requests"] = [projectID, set, qnt]
+                            filter = {"username": username}
+                            update = {
+                                "$push": {"requests": [projectID, "checkout", set, qnt]}
+                            }
+                            users.update_one(filter, update)
                         print("successful checkout")
                         return 1
             if set == 2:
-                if document["hwSet2Availability"] > qnt:
-                    document["hwSet2Availabilty"] = 0
+                if document["hwSet2Availability"] < qnt:
+                    leftover = document["hwSet2Availability"]
+                    filter = {"projectID": projectID}
+                    update = {
+                        "$set": {"hwSet2Availability": 0}
+                    }
+                    projects.update_one(filter, update)
                     for doc in users.find():
-                        user = document["username"]
+                        user = doc["username"]
                         if user == username:
-                            document["requests"] = [projectID, "checkout", set, qnt]
+                            filter = {"username": username}
+                            update = {
+                                "$push": {"requests": [[projectID, "checkout", set, leftover]]}
+                            }
+                            users.update_one(filter, update)
                     #should send message to frontend that quantity requested is more than that available
                     print("quantity requested is greater than that available, checked out all remaining units")
                     return 0
                 else:
-                    document["hwSet2Availability"] -= qnt
+                    currAvail = document["hwSet2Availability"]
+                    filter = {"projectID": projectID}
+                    update = {
+                        "$set": {"hwSet2Availability": currAvail - qnt}
+                    }
+                    projects.update_one(filter, update)
                     for doc in users.find():
                         user = doc["username"]
                         if user == username:
-                            doc["requests"] = [projectID, "checkout", set, qnt]
+                            filter = {"username": username}
+                            update = {
+                                "$push": {"requests": [projectID, "checkout", set, qnt]}
+                            }
+                            users.update_one(filter, update)
                         print("successful checkout")
                         return 1
-                    
+@app.route('/checkIn/<int:projectID>')
 def checkIn(projectID, set, qnt, username):
     for doc in users.find():
         name = doc["username"]
@@ -124,17 +163,40 @@ def checkIn(projectID, set, qnt, username):
                 projID = document["projectID"]
                 if projID == projectID:
                     if set == 1:
-                        document["hwSet1Availability"] += qnt
-                        doc["requests"] = [projectID, "checkin", set, qnt]
+                        currAvail = document["hwSet1Availability"]
+                        filter = {"projectID": projectID}
+                        currAvail += qnt
+                        update = {
+                            "$set": {"hwSet1Availability": currAvail}
+                        }
+                        projects.update_one(filter, update)
+                        filter = {"username": username}
+                        update = {
+                            "$push": {"requests": [projectID, "checkin", set, qnt]}
+                        }
+                        users.update_one(filter, update)
                         print("successful checkin")
                         return 1
                     else:
-                        document["hwSet2Availability"] += qnt
-                        doc["requests"] = [projectID, "checkin", set, qnt]
+                        currAvail = document["hwSet2Availability"]
+                        filter = {"projectID": projectID}
+                        currAvail += qnt
+                        update = {
+                            "$set": {"hwSet2Availability": currAvail}
+                        }
+                        projects.update_one(filter, update)
+                        filter = {"username": username}
+                        update = {
+                            "$push": {"requests": [projectID, "checkin", set, qnt]}
+                        }
+                        users.update_one(filter, update)
                         print("successful checkin")
                         return 1
 
-
-addUser("johndoe", "1234")
-checkUser("shriya", "1234")
+#if __name__ == '__main__':
+#    app.run(debug = True)
+#addUser("johndoe", "1234")
+#checkUser("shriya", "1234")
+#createProject("A1", "birdhouse", "birdhouse", 100, 100)
+checkIn("A1", 1, 55, "shriya")
 
