113f987b9aa53cc166c3a2d55012c4581a1fbc54
SV-Hello <sahilvartak8@gmail.com>
2023-11-05 15:01:25 -0600

    Complete revamp to fit project standards

--- src/components/AccessProjectPage.js
+++ src/components/AccessProjectPage.js
@@ -1,24 +1,130 @@
-import React from 'react';
-import Project from './Project.js'
+import React, {useState} from 'react';
+import BackgroundBox from './BackgroundBox.js';
 import {Link} from "react-router-dom"
+import {Button, TextField, Input} from '@mui/material';
+import './styles.css';
 
 const AccessProjectPage = () => {
+    const [name, setName] = useState('');
+    const [description, setDescr] = useState('');
+    const [newProjectID, setNewProjectID] = useState('');
+    const [existingProjectID, setExistingProjectID] = useState('');
 
-    return(
-        <div>
+    const updateName = (e) => {
+        setName(e.target.name);
+    }
+    const updateDecription = (e) => {
+        setDescr(e.target.description);
+    }
+    const updateNewProjectID = (e) => {
+        setNewProjectID(e.target.newProjectID);
+    }
+    const updateExistingProjectID = (e) => {
+        setExistingProjectID(e.target.existingProjectID);
+    }
 
-        <div className="title_font">
-              Projects
-        </div>
-          
-        <Project projName = 'Project Name 1'></Project>
-        <Project projName = 'Project Name 2'></Project>
-            <Link to="/">
-                    <button>Log out</button>
-        </Link>
-        </div>
+
+    
+    const createProject = () => {
+        
+    };
+
+    const useProject = () => {
+        
+    };
+
+    const tableStyle = {
+        borderCollapse: 'collapse',
+        width: '100'
+    };
+
+    const cellStyle = {
+        border: 'none',
+        width: 'fit-content',
+    };
+
+    return(<BackgroundBox backgroundColor = '#7FFFD4'>
+        <table style={tableStyle}>
+            <tbody>
+               
+                <td style={cellStyle}>            
+                    <div className="project_title_font">
+                        Create New Project
+                    </div>    
+
+                    <div className='input-container'>
+                        <div> Name &nbsp;
+                            <TextField
+                            value = {name}
+                            onChange = {updateName}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        </div>
+                        &nbsp;
+                        <div> Desrc. &nbsp;
+                            <TextField
+                            value = {description}
+                            onChange = {updateDecription}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        </div>
+                        &nbsp;
+                        <div> ProjID &nbsp;
+                            <TextField
+                            value = {newProjectID}
+                            onChange = {updateNewProjectID}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        </div>
+                    </div>
+
+                    <Button 
+                    color = "primary"  
+                    onClick = {createProject}
+                    variant="contained"
+                    >Create</Button>
+                </td>
+                
+                &nbsp;&nbsp;
+                
+                <td style={cellStyle}>            
+                    <div className="project_title_font">
+                        Use Existing Project
+                    </div>    
+
+                    <div> ProjID &nbsp;
+                            <TextField
+                            value = {existingProjectID}
+                            onChange = {updateExistingProjectID}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        
+                    </div>
+            
+                    <Button 
+                    color = "primary"  
+                    onClick = {useProject}
+                    variant="contained"
+                    >Use</Button>
+                </td>
+              
+            </tbody>
+    &nbsp;
+    &nbsp;
+    </table>
+
+   {/*Currently does nothing*/}
+   <Button 
+        color = "primary"   
+        variant="Contained"
+        >Back to Login Page</Button>
+
+    </BackgroundBox>
     )
-
 }
 
 export default AccessProjectPage;
