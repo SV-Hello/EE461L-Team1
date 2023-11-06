7979c72a1c70045d6fd720bb4d8fe5a63a528661
shriyab <shriya.B789@gmail.com>
2023-11-05 22:46:19 -0600

    front to back linking all functions

--- src/components/AccessProjectPage.js
+++ src/components/AccessProjectPage.js
@@ -1,24 +1,163 @@
-import React from 'react';
-import Project from './Project.js'
+import React, {useState} from 'react';
+import BackgroundBox from './BackgroundBox.js';
 import {Link} from "react-router-dom"
+import {Button, TextField, Input} from '@mui/material';
+import './styles.css';
+import axios from 'axios';
 
 const AccessProjectPage = () => {
+    const [name, setName] = useState('');
+    const [description, setDescr] = useState('');
+    const [newProjectID, setNewProjectID] = useState('');
+    const [existingProjectID, setExistingProjectID] = useState('');
 
-    return(
-        <div>
+    const updateName = (e) => {
+        setName(e.target.name);
+    }
+    const updateDecription = (e) => {
+        setDescr(e.target.description);
+    }
+    const updateNewProjectID = (e) => {
+        setNewProjectID(e.target.newProjectID);
+    }
+    const updateExistingProjectID = (e) => {
+        setExistingProjectID(e.target.existingProjectID);
+    }
 
-        <div className="title_font">
-              Projects
-        </div>
-          
-        <Project projName = 'Project Name 1'></Project>
-        <Project projName = 'Project Name 2'></Project>
-            <Link to="/">
-                    <button>Log out</button>
-        </Link>
-        </div>
+
+    
+    const createProject = () => {
+        axios({
+            method: "POST",
+            url: "/createproj",
+            data: {
+                id: existingProjectID,
+                description: description,
+                name: name,
+                hwSet1Cap: 100,
+                hwSet2Cap: 100
+            }
+        }).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            if (error.response) {
+              console.log(error.response)
+              console.log(error.response.status)
+              console.log(error.response.headers)
+              }
+          })
+    };
+
+    const useProject = () => {
+        axios({
+            method: "POST",
+            url: "/getProject",
+            data: {
+                id: existingProjectID
+            }
+        }).then((response) => {
+            console.log(response.data)
+        }).catch((error) => {
+            if (error.response) {
+              console.log(error.response)
+              console.log(error.response.status)
+              console.log(error.response.headers)
+            }
+        })
+    };
+
+    const tableStyle = {
+        borderCollapse: 'collapse',
+        width: '100'
+    };
+
+    const cellStyle = {
+        border: 'none',
+        width: 'fit-content',
+    };
+
+    return(<BackgroundBox backgroundColor = '#7FFFD4'>
+        <table style={tableStyle}>
+            <tbody>
+               
+                <td style={cellStyle}>            
+                    <div className="project_title_font">
+                        Create New Project
+                    </div>    
+
+                    <div className='input-container'>
+                        <div> Name &nbsp;
+                            <TextField
+                            value = {name}
+                            onChange = {updateName}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        </div>
+                        &nbsp;
+                        <div> Desrc. &nbsp;
+                            <TextField
+                            value = {description}
+                            onChange = {updateDecription}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        </div>
+                        &nbsp;
+                        <div> ProjID &nbsp;
+                            <TextField
+                            value = {newProjectID}
+                            onChange = {updateNewProjectID}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        </div>
+                    </div>
+
+                    <Button 
+                    color = "primary"  
+                    onClick = {createProject}
+                    variant="contained"
+                    >Create</Button>
+                </td>
+                
+                &nbsp;&nbsp;
+                
+                <td style={cellStyle}>            
+                    <div className="project_title_font">
+                        Use Existing Project
+                    </div>    
+
+                    <div> ProjID &nbsp;
+                            <TextField
+                            value = {existingProjectID}
+                            onChange = {updateExistingProjectID}
+                            type="text"
+                            variant="outlined"
+                            size="small"></TextField>
+                        
+                    </div>
+            
+                    <Button 
+                    color = "primary"  
+                    onClick = {useProject}
+                    variant="contained"
+                    >Use</Button>
+                </td>
+              
+            </tbody>
+    &nbsp;
+    &nbsp;
+    </table>
+
+   {/*Currently does nothing*/}
+   <Button 
+        color = "primary"   
+        variant="Contained"
+        >Back to Login Page</Button>
+
+    </BackgroundBox>
     )
-
 }
 
 export default AccessProjectPage;
--- src/components/HardwareSet.js
+++ src/components/HardwareSet.js
@@ -2,44 +2,73 @@ import React, {useState} from 'react';
 import {Input} from '@mui/material';
 import {Button} from '@mui/material';
 import './styles.css';
+import axios from 'axios';
 
 {/*Component that isn't Projects*/}
 function HardwareSet(props) {
-    const [quantity, setQuantity] = useState(100);
+    const [capacity, setCapacity] = useState(100);
+    const [quantity, setQuantity] = useState(capacity);
     const [input, setInput] = useState('');
 
     {/*Custom event handler to modify component's state*/}
     const updateInput = (e) => {
         setInput(e.target.value);
     }
-
-    const CheckIn = () => {
-        if(input == '') {
-            alert("Invalid value!")
-            return;
-        }
-        
-        let checking = parseInt(quantity) + parseInt(input);
-
-        if(checking > 100)
-            setQuantity(100);
-        else   
-            setQuantity(checking);
-    };
     
-    const CheckOut = () => {
+    function CheckIn() {
+        axios({
+            method: "POST",
+            url: "/checkIn",
+            data: {
+                set: props.hardwareNum,
+                qnt: input
+            }
+        }).then((response) => {
+            console.log(response.data)
+        }).catch((error) => {
+            if (error.response) {
+              console.log(error.response)
+              console.log(error.response.status)
+              console.log(error.response.headers)
+            }
+        })
+        if(response.data == "successful checkin"){
+            let checking = parseInt(quantity) + parseInt(input);
+            setQuantity(checking);
+        }    
+    }
+
+    function CheckOut() {
+        axios({
+            method: "POST",
+            url: "/checkOut",
+            data: {
+                projectID: hardwareData.id,
+                set: props.hardwareNum,
+                qnt: input
+            }
+        }).then((response) => {
+            console.log(response.data)
+        }).catch((error) => {
+            if (error.response) {
+              console.log(error.response)
+              console.log(error.response.status)
+              console.log(error.response.headers)
+            }
+        })
         if(input == '') {
             alert("Invalid value!")
             return;
         }
-
-        let checking = parseInt(quantity) - parseInt(input);
-
-        if(checking < 0)
-            setQuantity(0);
-        else   
+        if(response.data == "successful checkout"){
+            let checking = parseInt(quantity) - parseInt(input);
             setQuantity(checking);
-    };
+        }
+        else{
+            /*print quantity requested is greater than that available, checked out all remaining units*/
+            setQuantity(0);
+        }
+    }
 
     
     return(
@@ -47,10 +76,10 @@ function HardwareSet(props) {
             <div className="project_title_font">
                 HWSet
                 {props.hardwareNum || -1}
-                : {quantity} / 100
+                : {quantity} / {capacity}
              </div>
                     
-            {/*Material US Component: Input*/}
+        
              <Input 
                 placeholder="Enter qty"
                 type = "number"
@@ -59,7 +88,6 @@ function HardwareSet(props) {
              />
             &nbsp;
 
-            {/*Material US Component: Button*/}
             <Button 
                 color = "primary"  
                 onClick = {CheckIn}
